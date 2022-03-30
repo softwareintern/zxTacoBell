8a32d6851a38bebb841acf548c4d30e7a2bdda51
Tim Brockman <timBrockman@users.noreply.github.com>
2022-03-29 22:29:51 -0400

    Fixed indent

--- part1.js
+++ part1.js
@@ -7,7 +7,7 @@
  * w
  */
 (async function(){
-    const pOut = await $`touch foo.js; chmod +x foo.js; ls -l`;    
-    console.log(pOut);
-    console.log(pOut.stdout);
+  const pOut = await $`touch foo.js; chmod +x foo.js; ls -l`;    
+  console.log(pOut);
+  console.log(pOut.stdout);
 })();
