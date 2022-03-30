2a3cb60f4881617018a1dbe3923bb2def570c6c3
Tim Brockman <timBrockman@users.noreply.github.com>
2022-03-29 22:31:08 -0400

    Fixed indent

--- worm0.js
+++ worm0.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env zx
 /** Challenge: Create a Computer Worm in JS
- * run some commands and log the output
- * Warning!! Use some sort of counter to limit this.
- * Bonus: create one that can be used from a remote file.
- */
- const path = require('path');
- const wormName = path.basename(__filename, '.js');
- 
+* run some commands and log the output
+* Warning!! Use some sort of counter to limit this.
+* Bonus: create one that can be used from a remote file.
+*/
+const path = require('path');
+const wormName = path.basename(__filename, '.js');
+
 (async function(){
   const counter = Number(wormName.at(4)) + 1;
   if(counter <= 4){
